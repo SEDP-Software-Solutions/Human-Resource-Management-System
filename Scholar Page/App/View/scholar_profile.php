fb28c20f5378efd67e5d21b0a0c81a950ac1978f
arnold <arnoldayapana72@gmail.com>
2024-10-03 15:22:39 +0800

    Update

---
+++ Scholar Page/App/View/scholar_profile.php
@@ -0,0 +1,11 @@
+<?php
+$title = 'Scholar Compliance';
+$page = 'compliance';
+include('../../Core/Includes/header.php');
+?>
+<div class="col-md-11 shadow mt-7">
+    <h1 class="text-center bg-secondary mt-5">Hello Scholar</h1>
+</div>
+</body>
+
+</html>
